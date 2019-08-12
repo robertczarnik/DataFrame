b75e54dc4ee7afcca00e5d417cfb9bd287bc7d63
Robert Czarnik <robert.czarnik98@gmail.com>
2019-08-12 13:09:43 +0200

    update Main

--- DataFrame/src/Main.java
+++ DataFrame/src/Main.java
@@ -5,44 +5,17 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InconsistentTypeException {
-
-        String sciezka= "D:\\!!Studia\\Semestr III\\Java\\Pliki\\groupby.csv";
-
         DataFrame df = DataFrame.builder()
-                .file(sciezka)
+                .file(a[0])
                 .build();
 
-        DataFrameMultiThreads dfMT = DataFrameMultiThreads.builder()
-                .file(sciezka)
-                .build();
+        DataFrame grupa1 = df.groupby(new String[] {"id"}).max();
 
-
-        long startTime = System.nanoTime();
-        DataFrame.Grouping gr = df.groupby(new String[] {"id"});
-        DataFrame grupa1 = gr.max();
-        long endTime = System.nanoTime();
-        System.out.println("Jednowatkowy: "+(endTime - startTime)/1000000);
-
-        startTime = System.nanoTime();
-        DataFrameMultiThreads.Grouping gr2 = dfMT.groupby(new String[] {"id"});
-        DataFrame grupa2 = gr2.max();
-        endTime = System.nanoTime();
-        System.out.println("Wielowatkowy: "+(endTime - startTime)/1000000);
-
-        /*for (int i=0;i < grupa1.lista.get(0).size();i++){ // dane
+        for (int i=0;i < grupa1.lista.get(0).size();i++){ // dane
             for(int j=0;j<grupa1.lista.size();j++){
                 System.out.print(grupa1.lista.get(j).get(i).toString()+" ");
             }
             System.out.println(" ");
-        }*/
-
-        /*System.out.println("\n\n");
-
-        for (int i=0;i < grupa2.lista.get(0).size();i++){ // dane
-            for(int j=0;j<grupa2.lista.size();j++){
-                System.out.print(grupa2.lista.get(j).get(i).toString()+" ");
-            }
-            System.out.println(" ");
-        }*/
+        }
     }
 }
